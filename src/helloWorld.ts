0d10bc636f27e6338c217ad51b573e315d5d976f
y-kurami <yosuke.kurami@gmail.com>
2015-03-12 03:18:18 +0900

    Add tsd

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,4 +1,26 @@
-module Greeting{
+/// <reference path="../typings/lodash/lodash.d.ts" />
+
+module Greeting {
+  export function config (opt: {
+    /* this.name */
+    name: string;
+    includes?: string[];
+    excludes?: string[];
+  }) : void {
+
+  }
+
+
+  export class Pet {
+    a: string;
+    constructor (a1: string) {
+      this.a = a1;
+    }
+    concat (b: string) {
+      return this.a + b;
+    }
+  }
+
   export class Hello{
     constructor(private text : string){
     }
@@ -8,6 +30,17 @@ module Greeting{
   }
 }
 
+var arr:{name:string; key:number}[] = [{
+  name: 'foo', key:1
+}, {
+  name: 'bar', key:3
+}, {
+  name: 'foo', key:5
+}];
+
+console.log(_.groupBy(arr, 'name'));
+
 var hello : Greeting.Hello = new Greeting.Hello("Hello, World!");
 
 hello.say();
+
